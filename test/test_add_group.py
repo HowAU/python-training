4d1a0b259f5fa1ace05e0fc7720e604da30341e5
Kirill <kirtrishin@gmail.com>
2017-04-23 22:45:33 +0300

    Полная декомпозиция Группы

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import pytest
-
 from fixture.application import Application
 from model.group import Group
 
@@ -14,11 +13,11 @@ def app(request):
 
 def test_add_group(app):   #убираем метод Self и добавляем метод app для ссылки на фикстуру аппликешен
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="Gruppa", header="sdfbv", footer="asfasf"))
+    app.group.create(Group(name="Gruppa", header="sdfbv", footer="asfasf"))
     app.session.logout()
 
 
 def test_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
