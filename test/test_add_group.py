dd219f8fcafb10f3ae8256c41b5ed06e4fb7b939
Kirill <kirtrishin@gmail.com>
2017-04-23 22:34:22 +0300

    Выделили вспом класс

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from application import Application
+
+from fixture.application import Application
+from model.group import Group
 
 
 @pytest.fixture #команда инициализации фикстуры
@@ -12,12 +13,12 @@ def app(request):
 
 
 def test_add_group(app):   #убираем метод Self и добавляем метод app для ссылки на фикстуру аппликешен
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="Gruppa", header="sdfbv", footer="asfasf"))
-    app.logout()
+    app.session.logout()
 
 
 def test_empty_group(app):
-    app.login(username="", password="")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
