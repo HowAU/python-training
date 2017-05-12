fced92bbd1bf47c9f7542ef5db8d615613ec749c
Kirill <kirtrishin@gmail.com>
2017-05-12 22:48:59 +0300

    Проверка на размеры контактов

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,9 +2,12 @@ from model.contact import Contact
 
 
 def test_modify_contact_firstname(app):
+    old_contacts=app.contact.get_contact_list() #получаем список существующих групп
     if app.contact.count() == 0:
         app.contact.creating_the_contact(Contact(firstname="test1"))
     app.contact.modify_first_contact(Contact(firstname="New firstname"))
+    new_contacts = app.contact.get_contact_list()  # получаем список новых групп
+    assert len(old_contacts) == len(new_contacts) #сравниваем длины списков
 
 
 def test_modify_contact_mobile(app):
