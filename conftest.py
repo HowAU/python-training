0ae5a293b4dbc874e0a29431e89f839f940db1ca
Kirill <kirtrishin@gmail.com>
2017-05-25 07:38:14 +0300

    реализация запуска с нескольких браузеров

--- conftest.py
+++ conftest.py
@@ -7,11 +7,13 @@ fixture=None
                 #  в 1 окне
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser") #сделать ввод логина и пароля
+    baseUrl = request.config.getoption("--baseUrl")
     if fixture is None:
-        fixture = Application()               #непосредстевнно сама фикстура (набор вспомогательных методов)
+        fixture = Application(browser=browser, baseUrl=baseUrl)               #непосредстевнно сама фикстура (набор вспомогательных методов)
     else:
         if not fixture.is_valid():
-            fixture = Application()  # непосредстевнно сама фикстура (набор вспомогательных методов)
+            fixture = Application(browser=browser, baseUrl=baseUrl)  # непосредстевнно сама фикстура (набор вспомогательных методов)
     fixture.session.ensure_login(username="admin", password="secret") #проверка предусловия
     return fixture
 
@@ -22,3 +24,7 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin) #указание на разрушение фикстуры
     return fixture
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox") #что хотим передать, что с этим сделать и какое значение по умолчанию
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
