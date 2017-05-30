df66afd35708bb7112efab105f6482e7a10491a7
Kirill <kirtrishin@gmail.com>
2017-05-30 23:24:56 +0300

    Загрузка информации из кофиг файла

--- conftest.py
+++ conftest.py
@@ -1,20 +1,27 @@
 import pytest
+import json
+import os.path
 from fixture.application import Application
 
-fixture=None
+
+fixture = None
+target = None
 
 @pytest.fixture #команда инициализации фикстуры. добавка (scope = "session") дает возможность запускать несколько тестов
                 #  в 1 окне
 def app(request):
     global fixture
+    global target #указание на использование глобальной переменной
     browser = request.config.getoption("--browser") #сделать ввод логина и пароля
-    baseUrl = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Application(browser=browser, baseUrl=baseUrl)               #непосредстевнно сама фикстура (набор вспомогательных методов)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, baseUrl=baseUrl)  # непосредстевнно сама фикстура (набор вспомогательных методов)
-    fixture.session.ensure_login(username="admin", password="secret") #проверка предусловия
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption(
+            "--target"))  # переход к(абсолютный путь(путь к текущему файлу)). При данной записи нет нужды указывать адресс в настройках питона
+        with open(config_file) as f:
+            target = json.load(f) #f - указывает объект содержащийся в открытом файле
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, baseUrl=target['baseUrl'] )               #непосредстевнно сама фикстура (набор вспомогательных методов)
+   # непосредстевнно сама фикстура (набор вспомогательных методов)
+    fixture.session.ensure_login(username=target["username"], password=target["password"]) #проверка предусловия
     return fixture
 
 @pytest.fixture (scope="session", autouse=True)
@@ -27,4 +34,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox") #что хотим передать, что с этим сделать и какое значение по умолчанию
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
